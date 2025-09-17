1a11aa156ac199a293cbb816487aec90835efaa9
leejaeguen <worms0421@gmail.com>
2025-09-17 12:44:09 +0900

    영속성

--- 10_jpa/chap01-persistence-context-lecture-source/src/test/java/com/worms/section03/persistencecontext/EntityLifeCycleTest.java
+++ 10_jpa/chap01-persistence-context-lecture-source/src/test/java/com/worms/section03/persistencecontext/EntityLifeCycleTest.java
@@ -74,6 +74,100 @@ public class EntityLifeCycleTest {
         transaction.commit();
 
         Assertions.assertTrue(foundMenu == menuToRegist);
+    }
 
+    @Test
+    public void 영속성_객체_추가_및_변경_테스트() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Menu menuToRegist = new Menu();
+        menuToRegist.setMenuCode(501);
+        menuToRegist.setMenuName("수박죽");
+        menuToRegist.setMenuPrice(100000);
+        menuToRegist.setCategoryCode(10);
+        menuToRegist.setOrderableStatus("Y");
+
+        entityManager.persist(menuToRegist);        // 여기서부터 영속 상태가 됨
+        menuToRegist.setMenuName("메론죽");
+
+        Menu fondMenu = entityManager.find(Menu.class, 501);
+
+        transaction.commit();
+
+        Assertions.assertEquals("메론죽", fondMenu.getMenuName());
+    }
+
+    @Test
+    public void 준영속성_detach_테스트() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Menu foundMenu = entityManager.find(Menu.class, 11);        // select
+        Menu foundMenu2 = entityManager.find(Menu.class, 12);       // select
+        System.out.println("foundMenu = " + foundMenu);
+        System.out.println("foundMenu2 = " + foundMenu2);
+
+        /* 설명.
+         *  영속성 컨텍스트가 관리하던 엔티티 객체를 잠시 관리하지 않는 상태가 되게 한 것을 준영속 상태라고 한다.
+         *  detach, clear, close가 준영속 상태를 만들기 위한 메소드이다.
+         *
+         * 설명.
+         *   준영속을 사용하는 이유
+         *   : JPA에서 엔티티를 더 이상 영속성 컨텍스트에서 관리하지 않음을 의미하며,
+         *     특정 상황(flush를 특정 시점에 별도로 사용)에서 성능 최적화나
+         *     데이터 무결성 유지, 특정 작업 후 엔티티 변경 방지를 위해 사용 된다.
+         *     (DB에서 가져온 객체를 DB에 영향을 크게 주지 않는 선에서 활용하고 싶을 때, 둘 다 활용도 가능)
+        * */
+        entityManager.detach(foundMenu2);
+
+        foundMenu.setMenuPrice(7000);
+        foundMenu2.setMenuPrice(7000);
+
+
+        transaction.commit();
+    }
+
+    @Test
+    public void 준영속성_clear_close_테스트() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Menu foundMenu1 = entityManager.find(Menu.class, 11);
+        Menu foundMenu2 = entityManager.find(Menu.class, 12);
+
+        entityManager.clear();  // 영속성 컨텍스트에 있는 모든 영속 상태의 엔티티를 준영속으로 변경
+
+        foundMenu1.setMenuPrice(8000);
+        foundMenu2.setMenuPrice(8000);
+
+        transaction.commit();
+
+        Assertions.assertNotEquals(8000, entityManager.find(Menu.class, 11).getMenuPrice());
+        Assertions.assertNotEquals(8000, entityManager.find(Menu.class, 12).getMenuPrice());
+    }
+
+    @Test
+    public void 병합_merge_수정_테스트() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        Menu menuToDetch = entityManager.find(Menu.class, 5);
+        entityManager.clear();
+
+        menuToDetch.setMenuName("수박죽");
+
+        Menu refoundMenu = entityManager.find(Menu.class, 5);
+        System.out.println("첫 번째 5번 메뉴의 이름: " + menuToDetch.getMenuName());
+        System.out.println("두 번째 5번 메뉴의 이름: " + refoundMenu.getMenuName());
+
+        entityManager.merge(menuToDetch);
+
+        /* 설명. 결과적으로 준영속 살태였던 것과 DB에서 다시 조회한 것 중에 어떤 것이 살아남았는지 확인하기 위해 다시 find */
+        Menu managedMenu = entityManager.find(Menu.class, 5);
+
+        transaction.commit();
+
+        Assertions.assertEquals("수박죽", managedMenu.getMenuName());
     }
 }
