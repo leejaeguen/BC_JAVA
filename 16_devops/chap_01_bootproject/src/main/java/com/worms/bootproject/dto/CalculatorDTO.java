4b59793afb3ab6986c354ac29a363626a431e868
leejaeguen <worms0421@gmail.com>
2025-11-04 10:19:59 +0900

    db연동을 고려한 jpa

---
+++ 16_devops/chap_01_bootproject/src/main/java/com/worms/bootproject/dto/CalculatorDTO.java
@@ -0,0 +1,17 @@
+package com.worms.bootproject.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class CalculatorDTO {
+    @NonNull
+    private Integer num1;
+    @NonNull
+    private Integer num2;
+    private int sum = 0;
+}
